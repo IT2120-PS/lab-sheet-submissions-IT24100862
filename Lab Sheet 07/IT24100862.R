abd8993be7878bdd6c75c964122f296a30ce68f8
IT24100862 <IT24100862@my.sliit.lk>
2025-09-16 11:54:49 +0530

    Added LabSheet 7

---
+++ Lab Sheet 07/IT24100862.R
@@ -0,0 +1,6 @@
+punif(25, min = 0,max = 40,lower.tail = TRUE) - punif(10, min = 0,max = 40,lower.tail = TRUE)
+
+pexp(2,rate = 0.33333,lower.tail = TRUE)
+
+pnorm(130,100,15,lower.tail = FALSE)
+qnorm(0.95,100,15,lower.tail = TRUE)
